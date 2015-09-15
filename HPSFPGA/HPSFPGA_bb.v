9d8c175b830120ef736fd81cec0b46f6328d62cb
Gregory St-Jean <gregory.stjean@humanware.com>
2015-09-15 13:03:02 -0400

    FPGA project

---
+++ HPSFPGA/HPSFPGA_bb.v
@@ -0,0 +1,46 @@
+
+module HPSFPGA (
+	clk_clk,
+	reset_reset_n,
+	memory_mem_a,
+	memory_mem_ba,
+	memory_mem_ck,
+	memory_mem_ck_n,
+	memory_mem_cke,
+	memory_mem_cs_n,
+	memory_mem_ras_n,
+	memory_mem_cas_n,
+	memory_mem_we_n,
+	memory_mem_reset_n,
+	memory_mem_dq,
+	memory_mem_dqs,
+	memory_mem_dqs_n,
+	memory_mem_odt,
+	memory_mem_dm,
+	memory_oct_rzqin,
+	hps_0_h2f_reset_reset_n,
+	sw_external_connection_export,
+	led_external_connection_export);	
+
+	input		clk_clk;
+	input		reset_reset_n;
+	output	[14:0]	memory_mem_a;
+	output	[2:0]	memory_mem_ba;
+	output		memory_mem_ck;
+	output		memory_mem_ck_n;
+	output		memory_mem_cke;
+	output		memory_mem_cs_n;
+	output		memory_mem_ras_n;
+	output		memory_mem_cas_n;
+	output		memory_mem_we_n;
+	output		memory_mem_reset_n;
+	inout	[31:0]	memory_mem_dq;
+	inout	[3:0]	memory_mem_dqs;
+	inout	[3:0]	memory_mem_dqs_n;
+	output		memory_mem_odt;
+	output	[3:0]	memory_mem_dm;
+	input		memory_oct_rzqin;
+	output		hps_0_h2f_reset_reset_n;
+	input	[9:0]	sw_external_connection_export;
+	output	[9:0]	led_external_connection_export;
+endmodule
